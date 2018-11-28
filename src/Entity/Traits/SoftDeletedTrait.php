460306265f67812124e92ce8c264dcd6c13bdacb
benjamin <kazmierczakbenjamin@gmail.com>
2018-11-28 15:12:46 +0100

    add trait SoftDeleted

---
+++ src/Entity/Traits/SoftDeletedTrait.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 2018-11-28
+ * Time: 14:56
+ */
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait SoftDeletedTrait{
+
+    /**
+     * @var bool
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $deleted = false;
+
+    /**
+     * @return bool
+     */
+    public function isDeleted(): bool
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @param bool $deleted
+     * @return SoftDeletedTrait
+     */
+    public function setDeleted(bool $deleted): self
+    {
+        $this->deleted = $deleted;
+    }
+
+}
